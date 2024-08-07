8d8da09624fb29882e0f7568952b4cf371b9c35e
rainmc0123 <elsa.rgt22@gmail.com>
2024-08-07 11:17:03 +0700

    Update fixyarn.sh

--- fixyarn.sh
+++ fixyarn.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 # Membuat direktori untuk keyrings jika belum ada
-
 echo -e "\033[34m𝗣𝗥𝗢𝗦𝗘𝗦, 𝗦𝗜𝗟𝗔𝗛𝗞𝗔𝗡 𝗗𝗜𝗧𝗨𝗡𝗚𝗚𝗨\033[0m"
-sudo mkdir -p /etc/apt/keyrings
+sudo mkdir -p /etc/apt/keyrings > /dev/null 2>&1
 
 # Mengunduh dan menyimpan key GPG NodeSource
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg > /dev/null 2>&1
 
 # Menambahkan repository NodeSource ke sources.list.d
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_16.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_16.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list > /dev/null 2>&1
 
 # Memperbarui daftar paket dan menginstal Node.js
 sudo apt update > /dev/null 2>&1
@@ -19,7 +18,7 @@ sudo apt install -y nodejs > /dev/null 2>&1
 npm i -g yarn > /dev/null 2>&1
 
 # Pindah ke direktori aplikasi dan membangun
-cd /var/www/pterodactyl
+cd /var/www/pterodactyl || { echo "Direktori tidak ditemukan"; exit 1; }
 
 # Menginstal dependensi dan membangun aplikasi
 yarn > /dev/null 2>&1
